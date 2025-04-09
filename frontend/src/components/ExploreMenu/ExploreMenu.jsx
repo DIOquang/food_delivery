29398be47878e0fc4d9bb210c32064ca61cb19b8
pmquang <minhquangkzd03@gmail.com>
2025-04-09 14:17:05 +0700

    frontend_home_page

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -3,6 +3,8 @@ import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = ({category, setCategory}) => {
+
+
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our menu</h1>
@@ -10,8 +12,8 @@ const ExploreMenu = ({category, setCategory}) => {
         <div className="explore-menu-list">
             {menu_list.map((item, index) => {
                 return (
-                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-                        <img className={category===item.menu_name?"active": ""} src={item.menu_image} alt={item.menu_name} />
+                    <div onClick={() => setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
                 )
@@ -20,6 +22,6 @@ const ExploreMenu = ({category, setCategory}) => {
         <hr />
     </div>
   )
-}
+}  
 
 export default ExploreMenu
--- frontend1/src/main.jsx
+++ frontend1/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -10,5 +10,5 @@ createRoot(document.getElementById('root')).render(
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
 )
