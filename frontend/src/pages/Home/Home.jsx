f7b8e1ee5e30be3ed4fe9813d9f5763a4c5f2d2a
pmquang <minhquangkzd03@gmail.com>
2025-04-11 19:25:29 +0700

    done_frontend_setup_backend_database

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
-  const [categoty, setCategory] = useState("All");
+    const [category, setCategory] = useState('All');
 
   return (
     <div>
-      <Header/>
-      <ExploreMenu categoty={categoty} setCategory={setCategory} />
-      <FoodDisplay categoty={categoty} />
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   )
 }
