ab8a79f4baa303170617357e266b740ba80badc0
pmquang <minhquangkzd03@gmail.com>
2025-05-09 15:31:00 +0700

    Updete

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,378 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Cuốn",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Tráng miệng",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Bánh mì",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Bánh",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Chay thuần",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Mì Ý",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Mì",
+        menu_image: menu_8
+    }
+];
+
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Salad Hy Lạp",
+        image: food_1,
+        price: 120000,
+        description: "Salad Hy Lạp giàu chất xơ và vitamin, tốt cho sức khỏe tim mạch.",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Salad rau củ",
+        image: food_2,
+        price: 180000,
+        description: "Salad rau củ tươi mát, cung cấp nhiều dưỡng chất thiết yếu.",
+        category: "Salad"
+    },
+    {
+        _id: "3",
+        name: "Salad cỏ ba lá",
+        image: food_3,
+        price: 160000,
+        description: "Salad cỏ ba lá thanh mát, giúp thanh lọc cơ thể.",
+        category: "Salad"
+    },
+    {
+        _id: "4",
+        name: "Salad gà",
+        image: food_4,
+        price: 240000,
+        description: "Salad gà giàu đạm, hỗ trợ phát triển cơ bắp.",
+        category: "Salad"
+    },
+    {
+        _id: "5",
+        name: "Cuộn Lasagna",
+        image: food_5,
+        price: 140000,
+        description: "Món cuốn Lasagna thơm ngon với lớp phô mai béo ngậy.",
+        category: "Cuốn"
+    },
+    {
+        _id: "6",
+        name: "Cuộn Peri Peri",
+        image: food_6,
+        price: 120000,
+        description: "Cuốn Peri Peri cay nồng, kích thích vị giác.",
+        category: "Cuốn"
+    },
+    {
+        _id: "7",
+        name: "Cuộn gà",
+        image: food_7,
+        price: 200000,
+        description: "Cuốn gà mềm thơm, giàu protein.",
+        category: "Cuốn"
+    },
+    {
+        _id: "8",
+        name: "Cuộn chay",
+        image: food_8,
+        price: 150000,
+        description: "Cuốn rau củ nhẹ nhàng, thích hợp cho người ăn chay.",
+        category: "Cuốn"
+    },
+    {
+        _id: "9",
+        name: "Kem Ripple",
+        image: food_9,
+        price: 140000,
+        description: "Kem Ripple mát lạnh với hương vị cuốn hút.",
+        category: "Tráng miệng"
+    },
+    {
+        _id: "10",
+        name: "Kem trái cây",
+        image: food_10,
+        price: 220000,
+        description: "Kem trái cây thơm ngon, bổ sung vitamin tự nhiên.",
+        category: "Tráng miệng"
+    },
+    {
+        _id: "11",
+        name: "Kem hũ",
+        image: food_11,
+        price: 100000,
+        description: "Kem hũ tiện lợi, hương vị ngọt ngào.",
+        category: "Tráng miệng"
+    },
+    {
+        _id: "12",
+        name: "Kem vani",
+        image: food_12,
+        price: 120000,
+        description: "Kem vani cổ điển, thơm béo và mịn mát.",
+        category: "Tráng miệng"
+    },
+    {
+        _id: "13",
+        name: "Bánh mì gà",
+        image: food_13,
+        price: 120000,
+        description: "Bánh mì gà ngon miệng, giàu protein.",
+        category: "Bánh mì"
+    },
+    {
+        _id: "14",
+        name: "Bánh mì chay",
+        image: food_14,
+        price: 180000,
+        description: "Bánh mì chay lành mạnh, phù hợp với người ăn thuần chay.",
+        category: "Bánh mì"
+    },
+    {
+        _id: "15",
+        name: "Bánh mì nướng",
+        image: food_15,
+        price: 160000,
+        description: "Bánh mì nướng giòn rụm với nhân hấp dẫn.",
+        category: "Bánh mì"
+    },
+    {
+        _id: "16",
+        name: "Bánh mì lát",
+        image: food_16,
+        price: 240000,
+        description: "Bánh mì kẹp cổ điển, dễ ăn và tiện lợi.",
+        category: "Bánh mì"
+    },
+    {
+        _id: "17",
+        name: "Bánh cupcake",
+        image: food_17,
+        price: 140000,
+        description: "Bánh cupcake nhỏ xinh, ngọt ngào và mềm mịn.",
+        category: "Bánh"
+    },
+    {
+        _id: "18",
+        name: "Bánh chay",
+        image: food_18,
+        price: 120000,
+        description: "Bánh chay thơm ngon, không sử dụng sản phẩm động vật.",
+        category: "Bánh"
+    },
+    {
+        _id: "19",
+        name: "Bánh bơ đường",
+        image: food_19,
+        price: 200000,
+        description: "Bánh bơ đường ngọt ngào, thơm béo.",
+        category: "Bánh"
+    },
+    {
+        _id: "20",
+        name: "Bánh lát",
+        image: food_20,
+        price: 150000,
+        description: "Bánh lát tiện lợi, thích hợp cho mọi dịp.",
+        category: "Bánh"
+    },
+    {
+        _id: "21",
+        name: "Nấm xào tỏi",
+        image: food_21,
+        price: 140000,
+        description: "Nấm xào tỏi thơm nức, giàu dinh dưỡng.",
+        category: "Chay thuần"
+    },
+    {
+        _id: "22",
+        name: "Súp lơ chiên",
+        image: food_22,
+        price: 220000,
+        description: "Súp lơ chiên giòn, món ăn chay hấp dẫn.",
+        category: "Chay thuần"
+    },
+    {
+        _id: "23",
+        name: "Cơm trộn rau củ",
+        image: food_23,
+        price: 100000,
+        description: "Cơm trộn rau củ thơm ngon, đậm đà hương vị.",
+        category: "Chay thuần"
+    },
+    {
+        _id: "24",
+        name: "Cơm bí ngòi",
+        image: food_24,
+        price: 120000,
+        description: "Cơm bí ngòi thanh mát, nhẹ bụng.",
+        category: "Chay thuần"
+    },
+    {
+        _id: "25",
+        name: "Mì phô mai",
+        image: food_25,
+        price: 120000,
+        description: "Mì ống phô mai béo ngậy, hấp dẫn mọi lứa tuổi.",
+        category: "Mì Ý"
+    },
+    {
+        _id: "26",
+        name: "Mì sốt cà chua",
+        image: food_26,
+        price: 180000,
+        description: "Mì ống sốt cà chua đậm đà, chua ngọt hài hòa.",
+        category: "Mì Ý"
+    },
+    {
+        _id: "27",
+        name: "Mì kem",
+        image: food_27,
+        price: 160000,
+        description: "Mì kem sánh mịn, đầy lôi cuốn.",
+        category: "Mì Ý"
+    },
+    {
+        _id: "28",
+        name: "Mì gà",
+        image: food_28,
+        price: 240000,
+        description: "Mì gà thơm ngon, cung cấp năng lượng dồi dào.",
+        category: "Mì Ý"
+    },
+    {
+        _id: "29",
+        name: "Mì bơ",
+        image: food_29,
+        price: 140000,
+        description: "Mì bơ mềm thơm, béo ngậy dễ ăn.",
+        category: "Mì"
+    },
+    {
+        _id: "30",
+        name: "Mì rau củ",
+        image: food_30,
+        price: 120000,
+        description: "Mì rau củ đầy đủ dưỡng chất, phù hợp mọi lứa tuổi.",
+        category: "Mì"
+    },
+    {
+        _id: "31",
+        name: "Mì Somen",
+        image: food_31,
+        price: 200000,
+        description: "Mì Somen Nhật Bản thanh mát, dễ tiêu hóa.",
+        category: "Mì"
+    },
+    {
+        _id: "32",
+        name: "Mì chín",
+        image: food_32,
+        price: 150000,
+        description: "Mì chín mềm, kết hợp hương vị đậm đà.",
+        category: "Mì"
+    }
+];
