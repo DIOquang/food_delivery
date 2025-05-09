83faaf6da3f502ceeb52ba441e58026bdd2a2be6
pmquang <minhquangkzd03@gmail.com>
2025-05-09 14:25:48 +0700

    Update

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Đặt món ăn yêu thích của bạn ở đây</h2>
+        <p>Lựa chọn từ thực đơn đa dạng với nhiều món ăn hấp dẫn được chế biến từ những nguyên liệu hảo hạng nhất</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
